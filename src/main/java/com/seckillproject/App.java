1fcacccef152642d38609415305a06f45dda2ecd
realnumber666 <781504900@qq.com>
2019-04-10 10:06:22 +0800

    Add mybatis and druid, suc to connect mysql.

--- src/main/java/com/seckillproject/App.java
+++ src/main/java/com/seckillproject/App.java
@@ -1,7 +1,12 @@
 package com.seckillproject;
 
+import com.seckillproject.dao.UserDOMapper;
+import com.seckillproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 // 自动加载配置到项目中
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.seckillproject"})
 @RestController
+@MapperScan("com.seckillproject.dao")
 public class App {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home() {
-        return "Hello World!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if (userDO == null) {
+            return "用户对象不存在";
+        } else {
+            return userDO.getName();
+        }
     }
 
     public static void main( String[] args ) {
